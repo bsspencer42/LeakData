89ac44476e27899eb9ea47f22d0d5fceef611dfe
bsspencer42 <bsspencer42@gmail.com>
2021-08-03 15:51:08 -0400

    Add cell voltages

--- ParseEOL.py
+++ ParseEOL.py
@@ -124,7 +124,7 @@ CellVoltageMin = "{:.2f}".format(float(Master["[Batteriestatus_Sollwertvorgaben]
 CellVoltageMax = "{:.2f}".format(float(Master["[Batteriestatus_Sollwertvorgaben]"]["U_ZellMax_Set_Batteriestatus"]))
 CellVoltageRange = "{:.2f}".format(float(Master["[Batteriestatus_Sollwertvorgaben]"]["dU_ZellMax_Set_Batteriestatus"]))
 
-#Temp/Voltage Check
+#Temp Check
 CellTempCheck = Master["[Batteriestatus_Pruefergebnisse]"]["Temperaturwerte iO/niO?"]
 CellTemp1 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T_Zell_Get[n] [0]"]))
 CellTemp2 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T_Zell_Get[n] [1]"]))
@@ -150,12 +150,117 @@ CellTemp21 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T
 CellTemp22 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T_Zell_Get[n] [21]"]))
 CellTemp23 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T_Zell_Get[n] [22]"]))
 CellTemp24 = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["T_Zell_Get[n] [23]"]))
+CellTempRange = "{:.2f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["dT_Zell_Calc"]))
+#Voltage Check
+CellVoltCheck = Master["[Batteriestatus_Pruefergebnisse]"]["Spannungswerte iO/niO?"]
+CellVolt1 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [0]"]))
+CellVolt2 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [1]"]))
+CellVolt3 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [2]"]))
+CellVolt4 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [3]"]))
+CellVolt5 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [4]"]))
+CellVolt6 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [5]"]))
+CellVolt7 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [6]"]))
+CellVolt8 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [7]"]))
+CellVolt9 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [8]"]))
+CellVolt10 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [9]"]))
+CellVolt11 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [10]"]))
+CellVolt12 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [11]"]))
+CellVolt13 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [12]"]))
+CellVolt14 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [13]"]))
+CellVolt15 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [14]"]))
+CellVolt16 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [15]"]))
+CellVolt17 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [16]"]))
+CellVolt18 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [17]"]))
+CellVolt19 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [18]"]))
+CellVolt20 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [19]"]))
+CellVolt21 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [20]"]))
+CellVolt22 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [21]"]))
+CellVolt23 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [22]"]))
+CellVolt24 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [23]"]))
+CellVolt25 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [24]"]))
+CellVolt26 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [25]"]))
+CellVolt27 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [26]"]))
+CellVolt28 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [27]"]))
+CellVolt29 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [28]"]))
+CellVolt30 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [29]"]))
+CellVolt31 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [30]"]))
+CellVolt32 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [31]"]))
+CellVolt33 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [32]"]))
+CellVolt34 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [33]"]))
+CellVolt35 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [34]"]))
+CellVolt36 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [35]"]))
+CellVolt37 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [36]"]))
+CellVolt38 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [37]"]))
+CellVolt39 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [38]"]))
+CellVolt40 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [39]"]))
+CellVolt41 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [40]"]))
+CellVolt42 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [41]"]))
+CellVolt43 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [42]"]))
+CellVolt44 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [43]"]))
+CellVolt45 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [44]"]))
+CellVolt46 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [45]"]))
+CellVolt47 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [46]"]))
+CellVolt48 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [47]"]))
+CellVolt49 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [48]"]))
+CellVolt50 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [49]"]))
+CellVolt51 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [50]"]))
+CellVolt52 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [51]"]))
+CellVolt53 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [52]"]))
+CellVolt54 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [53]"]))
+CellVolt55 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [54]"]))
+CellVolt56 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [55]"]))
+CellVolt57 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [56]"]))
+CellVolt58 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [57]"]))
+CellVolt59 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [58]"]))
+CellVolt60 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [59]"]))
+CellVolt61 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [60]"]))
+CellVolt62 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [61]"]))
+CellVolt63 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [62]"]))
+CellVolt64 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [63]"]))
+CellVolt65 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [64]"]))
+CellVolt66 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [65]"]))
+CellVolt67 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [66]"]))
+CellVolt68 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [67]"]))
+CellVolt69 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [68]"]))
+CellVolt70 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [69]"]))
+CellVolt71 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [70]"]))
+CellVolt72 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [71]"]))
+CellVolt73 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [72]"]))
+CellVolt74 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [73]"]))
+CellVolt75 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [74]"]))
+CellVolt76 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [75]"]))
+CellVolt77 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [76]"]))
+CellVolt78 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [77]"]))
+CellVolt79 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [78]"]))
+CellVolt80 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [79]"]))
+CellVolt81 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [80]"]))
+CellVolt82 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [81]"]))
+CellVolt83 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [82]"]))
+CellVolt84 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [83]"]))
+CellVolt85 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [84]"]))
+CellVolt86 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [85]"]))
+CellVolt87 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [86]"]))
+CellVolt88 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [87]"]))
+CellVolt89 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [88]"]))
+CellVolt90 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [89]"]))
+CellVolt91 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [90]"]))
+CellVolt92 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [91]"]))
+CellVolt93 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [92]"]))
+CellVolt94 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [93]"]))
+CellVolt95 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [94]"]))
+CellVolt96 = "{:.3f}".format(float(Master["[Batteriestatus_Pruefergebnisse]"]["U_Zell_Get[n] [95]"]))
+
+
 
 #List of values for output file
 myExcelData = [battery, partNum, batType, startDate, startTime, i_DTC_Count, initBCMe, initCMC1, initCMC2, initCMC3, initBL_BCMe, initBL_CMC1, initBL_CMC2, initBL_CMC3, batConfigPset, batVehiclePset, batTargetMarketPset, batConfigNamePset, batThermoPset, batNameVehiclePset, batNameTargetMarket, HW_NumCMC1, HW_NumCMC2, HW_NumCMC3,serial_CMC1,serial_CMC2,serial_CMC3,HW_NumBMCe,HW_serial_BMCe,SW_serial_BMCe,HW_serial_CMC1,HW_serial_CMC2,HW_serial_CMC3,SW_serial_CMC1,SW_serial_CMC2,SW_serial_CMC3,\
-               preFlashBCMeSW_Check,PostFlashBCMeSW_Check,dataSet_download_Check,PostDSDL_BCMeSW_Check,preFlashCMCSW_Check,PostFlashCMCSW_Check,SW_Check,CellTempMin,CellTempMax,CellTempRange,CellVoltageMin,CellVoltageMax,CellVoltageRange,CellTempCheck,CellTemp1,CellTemp2,CellTemp3,CellTemp4,CellTemp5,CellTemp6,CellTemp7,CellTemp8,CellTemp9,CellTemp10,CellTemp11,CellTemp12,CellTemp13,CellTemp14,CellTemp15,CellTemp16,CellTemp17,CellTemp18,CellTemp19,CellTemp20,CellTemp21,CellTemp22,CellTemp23,CellTemp24]
+               preFlashBCMeSW_Check,PostFlashBCMeSW_Check,dataSet_download_Check,PostDSDL_BCMeSW_Check,preFlashCMCSW_Check,PostFlashCMCSW_Check,SW_Check,CellTempMin,CellTempMax,CellTempRange,CellVoltageMin,CellVoltageMax,CellVoltageRange,CellTempCheck,CellTemp1,CellTemp2,CellTemp3,CellTemp4,CellTemp5,CellTemp6,CellTemp7,CellTemp8,CellTemp9,CellTemp10,CellTemp11,CellTemp12,CellTemp13,CellTemp14,CellTemp15,CellTemp16,CellTemp17,CellTemp18,CellTemp19,CellTemp20,CellTemp21,CellTemp22,CellTemp23,CellTemp24,\
+               CellTempRange,CellVolt1,CellVolt2,CellVolt3,CellVolt4,CellVolt5,CellVolt6,CellVolt7,CellVolt8,CellVolt9,CellVolt10,CellVolt11,CellVolt12,CellVolt13,CellVolt14,CellVolt15,CellVolt16,CellVolt17,CellVolt18,CellVolt19,CellVolt20,CellVolt21,CellVolt22,CellVolt23,CellVolt24,CellVolt25,CellVolt26,CellVolt27,CellVolt28,CellVolt29,CellVolt30,CellVolt31,CellVolt32,CellVolt33,CellVolt34,CellVolt35,CellVolt36,CellVolt37,CellVolt38,CellVolt39,CellVolt40,CellVolt41,CellVolt42,CellVolt43,CellVolt44,CellVolt45,CellVolt46,CellVolt47,CellVolt48,CellVolt49,CellVolt50,CellVolt51,CellVolt52,CellVolt53,CellVolt54,CellVolt55,CellVolt56,CellVolt57,CellVolt58,CellVolt59,CellVolt60,CellVolt61,CellVolt62,CellVolt63,CellVolt64,CellVolt65,CellVolt66,CellVolt67,CellVolt68,CellVolt69,CellVolt70,CellVolt71,CellVolt72,CellVolt73,CellVolt74,CellVolt75,CellVolt76,CellVolt77,CellVolt78,CellVolt79,CellVolt80,CellVolt81,CellVolt82,CellVolt83,CellVolt84,CellVolt85,CellVolt86,CellVolt87,CellVolt88,CellVolt89,CellVolt90,CellVolt91,CellVolt92,CellVolt93,CellVolt94,CellVolt95,CellVolt96,\
+               ]
 myHeaders = ["Battery","Part Number", "Type","Date","Start Time","DTC Count","BCMe Begin SW","CMC1 Begin SW","CMC2 Begin SW","CMC3 Begin SW","BCMe Begin BL","CMC1 Begin BL","CMC2 Begin BL","CMC3 Begin BL","BatConfigPSet","VehiclePSet","TargetMarket","ConfigNamePSet","ThermoPSet","NameVehiclePSet","NameTargetMarket","CMC1 HW","CMC2 HW","CMC3 HW","CMC1 Serial","CMC2 Serial","CMC3 Serial","BCMe HW", "BCMe HW Serial", "BCMe SW Serial", "CMC1 HW Serial","CMC2 HW Serial","CMC3 HW Serial","CMC1 SW Serial","CMC2 SW Serial","CMC3 SW Serial",\
-             "preFlashBCMeSW_Check","PostFlashBCMeSW_Check","dataSet_download_Check","PostDSDL_BCMeSW_Check","preFlashCMCSW_Check","PostFlashCMCSW_Check","SW_Check","CellTempMin","CellTempMax","CellTempRange","CellVoltageMin","CellVoltageMax","CellVoltageRange","CellTempCheck","CellTemp1","CellTemp2","CellTemp3","CellTemp4","CellTemp5","CellTemp6","CellTemp7","CellTemp8","CellTemp9","CellTemp10","CellTemp11","CellTemp12","CellTemp13","CellTemp14","CellTemp15","CellTemp16","CellTemp17","CellTemp18","CellTemp19","CellTemp20","CellTemp21","CellTemp22","CellTemp23","CellTemp24"]
+             "preFlashBCMeSW_Check","PostFlashBCMeSW_Check","dataSet_download_Check","PostDSDL_BCMeSW_Check","preFlashCMCSW_Check","PostFlashCMCSW_Check","SW_Check","CellTempMin","CellTempMax","CellTempRange","CellVoltageMin","CellVoltageMax","CellVoltageRange","CellTempCheck","CellTemp1","CellTemp2","CellTemp3","CellTemp4","CellTemp5","CellTemp6","CellTemp7","CellTemp8","CellTemp9","CellTemp10","CellTemp11","CellTemp12","CellTemp13","CellTemp14","CellTemp15","CellTemp16","CellTemp17","CellTemp18","CellTemp19","CellTemp20","CellTemp21","CellTemp22","CellTemp23","CellTemp24",\
+             "CellTempRange","CellVolt1","CellVolt2","CellVolt3","CellVolt4","CellVolt5","CellVolt6","CellVolt7","CellVolt8","CellVolt9","CellVolt10","CellVolt11","CellVolt12","CellVolt13","CellVolt14","CellVolt15","CellVolt16","CellVolt17","CellVolt18","CellVolt19","CellVolt20","CellVolt21","CellVolt22","CellVolt23","CellVolt24","CellVolt25","CellVolt26","CellVolt27","CellVolt28","CellVolt29","CellVolt30","CellVolt31","CellVolt32","CellVolt33","CellVolt34","CellVolt35","CellVolt36","CellVolt37","CellVolt38","CellVolt39","CellVolt40","CellVolt41","CellVolt42","CellVolt43","CellVolt44","CellVolt45","CellVolt46","CellVolt47","CellVolt48","CellVolt49","CellVolt50","CellVolt51","CellVolt52","CellVolt53","CellVolt54","CellVolt55","CellVolt56","CellVolt57","CellVolt58","CellVolt59","CellVolt60","CellVolt61","CellVolt62","CellVolt63","CellVolt64","CellVolt65","CellVolt66","CellVolt67","CellVolt68","CellVolt69","CellVolt70","CellVolt71","CellVolt72","CellVolt73","CellVolt74","CellVolt75","CellVolt76","CellVolt77","CellVolt78","CellVolt79","CellVolt80","CellVolt81","CellVolt82","CellVolt83","CellVolt84","CellVolt85","CellVolt86","CellVolt87","CellVolt88","CellVolt89","CellVolt90","CellVolt91","CellVolt92","CellVolt93","CellVolt94","CellVolt95","CellVolt96",\
+             ]
 count = 0
 
 for header in myHeaders:
